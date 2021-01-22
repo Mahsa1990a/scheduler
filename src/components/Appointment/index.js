5776a575c9446647b5d75430b8c15a6264430b8c
Mahsa Arabameri <amerimahsa@yahoo.com>
2021-01-21 19:15:58 -0500

    using our custom hook

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -2,15 +2,52 @@ import React from "react";
 import Header from "./Header";
 import Show from "./Show";
 import Empty from "./Empty";
+import useVisualMode from "../../hooks/useVisualMode";
+import Form from "./Form";
 
 import "components/Appointment/styles.scss";
 
+
+
 export default function Appointment (props) {
+
+  const EMPTY = "EMPTY";
+  const SHOW = "SHOW";
+  const CREATE = "CREATE";
+
+  const { mode, transition, back } = useVisualMode(
+    props.interview ? SHOW : EMPTY
+  ); 
 
   return (
     <article className="appointment">
       <Header time={props.time}/>
-      {props.interview ? <Show student={props.interview.student} interviewer={props.interview.interviewer}/> : <Empty />}
+      {/* {props.interview ? <Show student={props.interview.student} interviewer={props.interview.interviewer}/> : <Empty />} */}
+      {mode === EMPTY && <Empty onAdd={() => transition(CREATE)} />}
+      {mode === SHOW && (
+        <Show
+          student={props.interview.student}
+          interviewer={props.interview.interviewer.name}
+          onEdit={transition("onEdit")}
+          onDelete={transition("onDelete")}
+        />
+      )}
+      {mode === CREATE && (
+        <Form 
+        interviewers={[]}
+        // onSave={transition("onSave")}
+         onCancel={() => back("onCancel")}
+        />
+      )
+
+      }
+      {/* {mode === CREATE && (
+        <Form  
+        interviewers={props.interviewers} 
+        onClick={props.onSave}
+        onClick={props.onCancel}
+        />)} */}
     </article>
+
   )
 }
