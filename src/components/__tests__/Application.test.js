2d3736ac4e9b5a7c37c98f515dc8fc22ebe81b83
Mahsa Arabameri <amerimahsa@yahoo.com>
2021-01-25 18:52:46 -0500

    added waitForElement function

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -1,11 +1,19 @@
 import React from "react";
 
-import { render, cleanup } from "@testing-library/react";
+import { render, cleanup, waitForElement } from "@testing-library/react";
 
 import Application from "components/Application";
 
 afterEach(cleanup);
 
-it("renders without crashing", () => {
-  render(<Application />);
+it("defaults to Monday and changes the schedule when a new day is selected", () => {
+ 
+  //we need to be asynchronous
+  //wait for the fake API request to complete before we confirm that the data has loaded
+  //Before we request the data from the API, we won't render any days
+
+  //Use waitForElement to wait for the text "Monday" to appear in the document
+
+  const { getByText } = render(<Application />);
+  return waitForElement(() => getByText("Monday"));
 });
