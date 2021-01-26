3f7a156d9d0e3593e5b460876f643a3a7c838936
Mahsa Arabameri <amerimahsa@yahoo.com>
2021-01-26 13:38:42 -0500

    added test id

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -28,7 +28,11 @@ export default function DayListItem(props) {
 //setDay is a call back that takes in an argument,  so you need to wrap it in an anonymous func
 //setDay:Function accepts the name of the day eg. "Monday", "Tuesday"
   return (
-    <li className={dayClass} onClick={() => props.setDay(props.name)}>
+    <li 
+      className={dayClass} 
+      onClick={() => props.setDay(props.name)}
+      data-testid="day"
+      >
       <h2 className="text--regular">{props.name}</h2>
       <h3 className="text--light">{formatSpots(props.spots)}</h3>
     </li>
