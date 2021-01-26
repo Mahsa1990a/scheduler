6f589006cdd2c4cf6f55d9771fd68649ecd09d14
Mahsa Arabameri <amerimahsa@yahoo.com>
2021-01-25 19:40:30 -0500

    Mocking Modules

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -1,19 +1,27 @@
 import React from "react";
+import Application from "components/Application";
 
 import { render, cleanup, waitForElement } from "@testing-library/react";
+import { fireEvent } from "@testing-library/react/dist";
 
-import Application from "components/Application";
 
 afterEach(cleanup);
 
-it("defaults to Monday and changes the schedule when a new day is selected", () => {
- 
-  //we need to be asynchronous
-  //wait for the fake API request to complete before we confirm that the data has loaded
-  //Before we request the data from the API, we won't render any days
+describe("Application", () => {
+  it("defaults to Monday and changes the schedule when a new day is selected", () => {
+  
+    //we need to be asynchronous
+    //wait for the fake API request to complete before we confirm that the data has loaded
+    //Before we request the data from the API, we won't render any days
 
-  //Use waitForElement to wait for the text "Monday" to appear in the document
+    //Use waitForElement to wait for the text "Monday" to appear in the document
 
-  const { getByText } = render(<Application />);
-  return waitForElement(() => getByText("Monday"));
+    const { getByText } = render(<Application />);
+    //We can make our test asynchronous by returning a Promise
+    return waitForElement(() => getByText("Monday"))
+      .then(() => {
+        fireEvent.click(getByText("Tuesday"));
+        expect(getByText("Leopold Silvers")).toBeInTheDocument();
+      });
+  });
 });
