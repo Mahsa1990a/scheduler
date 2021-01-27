21a36c02e6aa621c84c9a07049bd11dfc6fcb96b
Mahsa Arabameri <amerimahsa@yahoo.com>
2021-01-27 08:54:01 -0500

    Refactoring the Test

--- cypress/integration/navigation.spec.js
+++ cypress/integration/navigation.spec.js
@@ -6,11 +6,15 @@ describe("Navigation", () => {
 
   it("should navigate to Tuesday", () => {
     cy.visit("/");
-    cy.get("li")
-      .contains("Tuesday")
-      .click()
+    // cy.get("li")
+    //   .contains("Tuesday")
+    //   .click()
       
+    // cy.contains("li", "Tuesday")
+    //   .should("have.css", "background-color", "rgb(242, 242, 242)"); All of them refactored to :
+
     cy.contains("li", "Tuesday")
-      .should("have.css", "background-color", "rgb(242, 242, 242)"); 
+      .click()
+      .should("have.css", "background-color", "rgb(242, 242, 242)");
   });
 });
