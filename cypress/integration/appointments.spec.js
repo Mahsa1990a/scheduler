f461f6bec32c23e116ac8e726a1a167f1de0b868
Mahsa Arabameri <amerimahsa@yahoo.com>
2021-01-27 13:26:19 -0500

    Added appointment test and test for click Add button

--- cypress/integration/appointments.spec.js
+++ cypress/integration/appointments.spec.js
@@ -3,7 +3,12 @@ describe("Appointments", () => {
   it("should book an interview", () => {
     cy.visit("/")
     cy.contains("Monday")
-    
+    // cy.get(':nth-child(2) > .appointment__add > .appointment__add-button').click(); OR :
+    cy.get("[alt=Add]")
+      .first()
+      .click();
+
+
   });
 
   
