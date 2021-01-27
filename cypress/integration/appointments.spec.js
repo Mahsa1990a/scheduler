68f5abb4e2d6924ae6513b92306fb116ae18cca0
Mahsa Arabameri <amerimahsa@yahoo.com>
2021-01-27 13:36:48 -0500

    Clicks the save button

--- cypress/integration/appointments.spec.js
+++ cypress/integration/appointments.spec.js
@@ -3,14 +3,21 @@ describe("Appointments", () => {
   it("should book an interview", () => {
     cy.visit("/")
     cy.contains("Monday")
+
+    //Clicks on the "Add" button in the second appointment
     // cy.get(':nth-child(2) > .appointment__add > .appointment__add-button').click(); OR :
     cy.get("[alt=Add]")
       .first()
       .click();
 
+    //Enters their name
     cy.get('[data-testid=student-name-input]').type("Lydia Miller-Jones");
 
+    //Chooses an interviewer
     cy.get("[alt='Sylvia Palmer']").click();
+
+    //Clicks the save button
+    cy.contains("Save").click();
   });
 
   
