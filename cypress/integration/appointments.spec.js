d75e328b27af8ab34892676eeef7652088530cab
Mahsa Arabameri <amerimahsa@yahoo.com>
2021-01-27 13:31:39 -0500

    Chooses an interviewer

--- cypress/integration/appointments.spec.js
+++ cypress/integration/appointments.spec.js
@@ -9,6 +9,8 @@ describe("Appointments", () => {
       .click();
 
     cy.get('[data-testid=student-name-input]').type("Lydia Miller-Jones");
+
+    cy.get("[alt='Sylvia Palmer']").click();
   });
 
   
